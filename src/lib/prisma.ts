0ce3be857fb1dc96d944a1ba3617c0b0a1f1d797
Gustavo067 <gustavo067dev@gmail.com>
2026-06-21 09:08:20 -0300

    Criando usuarios

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "node:process";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({
+    adapter,
+    log: env.NODE_ENV === 'dev' ? ["query"] : []
+});
