4b44b0755f6754af3916230abe475f044a2192e0
Gustavo Medeiros <78024109+Gustavo067@users.noreply.github.com>
2026-06-29 08:55:41 -0300

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from "fastify";
 import { routes  } from "./http/routes.js";
+import { ZodError } from "zod";
+import { env } from "./env/index.js";
 
 export const app = fastify();
 
 app.register(routes);
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: "Validation error",
+            issues: error.format()
+        });
+    }
+
+    if (env.NODE_ENV !== "production") {
+        console.error(error);
+    }else{
+
+    }
+
+    return reply.status(500).send({
+        message: "Internal server error"
+    });
+});
