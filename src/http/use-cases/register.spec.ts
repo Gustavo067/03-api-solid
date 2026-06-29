9cf796887a36febf6348e065504034fef4ae1f03
Gustavo Medeiros <78024109+Gustavo067@users.noreply.github.com>
2026-06-29 10:03:06 -0300

    Add files via upload

---
+++ src/http/use-cases/register.spec.ts
@@ -0,0 +1,30 @@
+import { expect, describe, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { PrismaUsersRepository } from "../repositories/prisma/prisma-users-repository";
+import { compare } from "bcryptjs";
+
+describe("Register Use Case", () => {
+    it("should hash user password upon registration", async () => {
+        const prismaUsersRepository = new PrismaUsersRepository();
+        const registerUseCase = new RegisterUseCase({
+            create: async (data) => ({
+                id: "1",
+                name: data.name,
+                email: data.email,
+                password_hash: data.password_hash,
+                created_at: new Date()
+            }),
+            findByEmail: async () => null
+        });
+
+       const { user } = await registerUseCase.execute({
+            name: "John Doe",
+            email: "john.doe@example.com",
+            password: "123456"
+        });
+
+       const isPasswordCorrectlyHashed = await compare("123456", user.password_hash);
+
+       expect(isPasswordCorrectlyHashed).toBe(true)
+    });
+});
